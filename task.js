c5b589a5198d7a0319c5184377d91f5dd14a9cf0
Vaibhav Rathore <vaibhavrathorema@gmail.com>
2022-09-11 18:16:17 +0530

    change response

--- task.js
+++ task.js
@@ -3,17 +3,48 @@ const axios = require("axios");
 const app = express();
 
 app.get("/todos", async (req, res) => {
-  const data = await axios("https://jsonplaceholder.typicode.com/todos");
-  res.send(data.data);
+  const todoAxiosResponse = await axios("https://jsonplaceholder.typicode.com/todos");
+  const todoList = await todoAxiosResponse.data.map(
+    (item) =>
+      (todata = {
+        id: item.id,
+        title: item.title,
+        completed: item.completed,
+      })
+  );
+  res.send(todoList);
 });
 
-app.get("/user/:id", async (req, res) => {
-    const user = await req.params.id
- const data = await axios(`https://jsonplaceholder.typicode.com/users/${user}`);
-res.send(data.data)
 
+app.get("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const userAxiosResponse = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+
+  const userDetails = { 
+    id : userAxiosResponse.data.id,
+    name : userAxiosResponse.data.name,
+    email : userAxiosResponse.data.email,
+    phone : userAxiosResponse.data.phone,
+  }
+
+  const userTodoAxiosResponse = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userId}/todos`
+  );
+  userDetails.todos = userTodoAxiosResponse.data.map(
+    (item) =>
+      (todata = {
+        id: item.id,
+        title: item.title,
+        userId: item.userId,
+        completed: item.completed,
+      })
+  );
+
+  res.send(userDetails);
 });
 
-app.listen(8000, () => {
-  console.log("server is running on 8000");
+app.listen(3000, () => {
+  console.log("server is running on 3000");
 });
